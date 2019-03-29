f6b3cd91696d412113929a1076979992a03f24dc
tat-tea <dream2catch@gmail.com>
2019-03-29 18:06:13 +0900

    Add files via upload

--- FunctionDisable_ver1.1/src/jp/co/ncast/form/FunctionDisableForm.java
+++ FunctionDisable_ver1.1/src/jp/co/ncast/form/FunctionDisableForm.java
@@ -15,6 +15,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import sfdc_rest.GeneratePackageXml;
+
 public class FunctionDisableForm extends JFrame implements ActionListener {
 
 	JTextArea textArea;
@@ -27,6 +29,7 @@ public class FunctionDisableForm extends JFrame implements ActionListener {
 	private static final String btn_file_choose = "btn_file_choose";
 	private static final String btn_disable = "btn_disable";
 	private static final String btn_enable = "btn_enable";
+	private static final String btn_pkgxml = "btn_pkgxml";
 
 	private JButton button_1;
 
@@ -52,69 +55,83 @@ public class FunctionDisableForm extends JFrame implements ActionListener {
 	public FunctionDisableForm() {
 		setTitle("機能無効化くん_ver1.0");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 460, 368);
+		setBounds(100, 100, 460, 483);
 		getContentPane().setLayout(null);
 
 		metaDirName = new JTextField();
-		metaDirName.setBounds(12, 27, 355, 19);
+		metaDirName.setBounds(12, 140, 355, 19);
 		getContentPane().add(metaDirName);
 		metaDirName.setColumns(10);
 
 		chckbx_Trigger = new JCheckBox("Apexトリガ");
 		chckbx_Trigger.setSelected(true);
-		chckbx_Trigger.setBounds(67, 102, 137, 21);
+		chckbx_Trigger.setBounds(67, 215, 137, 21);
 		getContentPane().add(chckbx_Trigger);
 
 		chckbx_Process = new JCheckBox("プロセスビルダ");
 		chckbx_Process.setSelected(true);
-		chckbx_Process.setBounds(67, 135, 137, 21);
+		chckbx_Process.setBounds(67, 248, 137, 21);
 		getContentPane().add(chckbx_Process);
 
 		chckbx_Workflow = new JCheckBox("ワークフロー");
 		chckbx_Workflow.setSelected(true);
-		chckbx_Workflow.setBounds(241, 135, 135, 21);
+		chckbx_Workflow.setBounds(241, 248, 135, 21);
 		getContentPane().add(chckbx_Workflow);
 
 		chckbx_Validation = new JCheckBox("入力規則");
 		chckbx_Validation.setSelected(true);
-		chckbx_Validation.setBounds(240, 103, 174, 21);
+		chckbx_Validation.setBounds(240, 216, 174, 21);
 		getContentPane().add(chckbx_Validation);
 
 		JLabel label = new JLabel("メタデータ格納先フォルダ");
-		label.setBounds(12, 10, 164, 13);
+		label.setBounds(12, 123, 164, 13);
 		getContentPane().add(label);
 
 		JButton button = new JButton("参照");
-		button.setBounds(373, 26, 61, 21);
+		button.setBounds(373, 139, 61, 21);
 		button.addActionListener(this);
 		button.setActionCommand(btn_file_choose);
 		getContentPane().add(button);
 
 		JLabel label_1 = new JLabel("無効化対象機能");
-		label_1.setBounds(12, 78, 103, 13);
+		label_1.setBounds(12, 191, 103, 13);
 		getContentPane().add(label_1);
 
 		textArea = new JTextArea();
 		textArea.setWrapStyleWord(true);
 		textArea.setLineWrap(true);
-		textArea.setBounds(12, 269, 422, 52);
+		textArea.setBounds(12, 382, 422, 52);
 		getContentPane().add(textArea);
 
 		JLabel label_2 = new JLabel("処理結果");
-		label_2.setBounds(12, 246, 78, 13);
+		label_2.setBounds(12, 359, 78, 13);
 		getContentPane().add(label_2);
 
 		button_1 = new JButton("無効化");
 		button_1.setActionCommand(btn_disable);
-		button_1.setBounds(67, 196, 131, 30);
+		button_1.setBounds(67, 309, 131, 30);
 		button_1.addActionListener(this);
 		getContentPane().add(button_1);
 
 		JButton btnGenerate = new JButton("有効化");
-		btnGenerate.setBounds(223, 196, 131, 30);
+		btnGenerate.setBounds(223, 309, 131, 30);
 		btnGenerate.setActionCommand(btn_enable);
 		btnGenerate.addActionListener(this);
 		getContentPane().add(btnGenerate);
+
+		JButton btnGeneratePkgXml = new JButton("package.xml生成");
+		btnGeneratePkgXml.setBounds(260, 51, 174, 30);
+		btnGeneratePkgXml.addActionListener(this);
+		btnGeneratePkgXml.setActionCommand(btn_pkgxml);
+		getContentPane().add(btnGeneratePkgXml);
+
+		JTextArea textArea_1 = new JTextArea();
+		textArea_1.setBounds(12, 35, 231, 22);
+		getContentPane().add(textArea_1);
+
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(12, 10, 50, 13);
+		getContentPane().add(lblNewLabel);
 
 	}
 
@@ -149,7 +166,8 @@ public class FunctionDisableForm extends JFrame implements ActionListener {
 				// 入力チェック
 				checkInput();
 
-				JOptionPane.showMessageDialog(null, "機能無効化リスト（success.csvを指定してください。）", "確認", JOptionPane.INFORMATION_MESSAGE, null);
+				JOptionPane.showMessageDialog(null, "機能無効化リスト（success.csvを指定してください。）", "確認",
+						JOptionPane.INFORMATION_MESSAGE, null);
 
 				// success.csvの選択
 				JFileChooser filechooser = new JFileChooser(metaDirName.getText());
@@ -169,6 +187,9 @@ public class FunctionDisableForm extends JFrame implements ActionListener {
 					action.editMetaData(true);
 					textArea.setText("正常に処理が終了しました。");
 				}
+			} else if (btn_pkgxml.equals(e.getActionCommand())) {
+				GeneratePackageXml generateXml = new GeneratePackageXml();
+				generateXml.generateXml();
 			}
 
 		} catch (Exception ex) {
